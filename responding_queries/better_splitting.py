5e1117b31689b1a2b5c4fa2ef372608071fc9c92
Çağrı Emre AKIN <cagriemreakin@gmail.com>
2017-01-03 21:45:22 +0200

    Create better_splitting.py
    splitting text with non characters with & without using regular expression

---
+++ responding_queries/better_splitting.py
@@ -0,0 +1,49 @@
+# 1 Gold Star
+
+# The built-in <string>.split() procedure works
+# okay, but fails to find all the words on a page
+# because it only uses whitespace to split the
+# string. To do better, we should also use punctuation
+# marks to split the page into words.
+
+# Define a procedure, split_string, that takes two
+# inputs: the string to split and a string containing
+# all of the characters considered separators. The
+# procedure should return a list of strings that break
+# the source string up by the characters in the
+# splitlist.
+
+#without regular expression
+def split_string(source,splitlist):
+    str = []
+    check = True
+    for key in source:
+        if key in splitlist:
+            check = True
+        else:
+            if check:
+                str.append(key)
+                check = False
+            else:
+                str[-1] = str[-1] + key
+                
+    return str
+
+#with regular expression
+#import re
+#def split_string(source,splitlist):
+#    return re.findall(r"[\w']+", source)
+
+
+
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print out
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+#out = split_string("After  the flood   ...  all the colors came out.", " .")
+#print out
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+#out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+#print out
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
